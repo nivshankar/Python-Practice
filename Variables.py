1f186396ac23d8be2a49c23a3c31c11157ccc4fd
Neev Shankar <nivshankar9@gmail.com>
2026-05-13 18:51:50 +0530

    Variable and type printing

---
+++ Variables.py
@@ -0,0 +1,22 @@
+x=10
+y=18.822
+name='Neev'
+boolean=True
+comp=3+7j
+print("\n\t",x,"->",type(x))
+print("\n\t",y,"->",type(y))
+print("\n\t",name,"->",type(name))
+print("\n\t",boolean,"->",type(boolean))
+print("\n\t",comp,"->",type(comp))
+'''
+Output:
+	 10 -> <class 'int'>
+
+	 18.822 -> <class 'float'>
+
+	 Neev -> <class 'str'>
+
+	 True -> <class 'bool'>
+
+	 (3+7j) -> <class 'complex'>
+'''
