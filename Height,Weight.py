f5b7fdf556bfff83bcafb21d8c9fde0a02582537
Neev Shankar <nivshankar9@gmail.com>
2026-05-13 18:59:29 +0530

    Height and weight of a person

---
+++ Height,Weight.py
@@ -0,0 +1,14 @@
+height=float(input('\n\tEnter you Height in cm : '))
+weight=float(input('\n\tEnter you Weight in Kg : '))
+print("\n\n\tYour Height is : ",height,"cm\n\tYour Weight is : ",weight,"Kg")
+'''
+Output:
+
+	Enter you Height in cm : 187
+
+	Enter you Weight in Kg : 75
+
+
+	Your Height is :  187.0 cm
+	Your Weight is :  75.0 Kg
+'''
