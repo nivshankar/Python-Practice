e24a81d832c8833c5b582c901baee9af34f78ad0
Neev Shankar <nivshankar9@gmail.com>
2026-05-13 18:41:02 +0530

    Calculation operations on 2 numbers

---
+++ Calculation.py
@@ -0,0 +1,31 @@
+x=float(input('\n\tEnter first number : '))
+y=float(input('\n\tEnter second number : '))
+print("\n\tAddition : ",x+y)
+print("\n\tSubstraction : ",x-y)
+print("\n\tMuliplication : ",x*y)
+print("\n\tDivision: ",x/y)
+print("\n\tModulus : ",x%y)
+print("\n\tFloor Division : ",x//y)
+print("\n\tExponential : ",x**y)
+'''
+Output:
+
+	Enter first number : 23
+
+	Enter second number : 3
+
+	Addition :  26.0
+
+	Substraction :  20.0
+
+	Muliplication :  69.0
+
+	Division:  7.666666666666667
+
+	Modulus :  2.0
+
+	Floor Division :  7.0
+
+	Exponential :  12167.0
+
+'''
