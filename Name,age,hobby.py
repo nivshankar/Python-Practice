c101b3f8e9c6675682cc869f5f129c7437c6b9b3
Neev Shankar <nivshankar9@gmail.com>
2026-05-13 18:25:01 +0530

    Name , age , hobby

---
+++ Name,age,hobby.py
@@ -0,0 +1,15 @@
+name=input('\n\tEnter your name : ')
+age=int(input('\n\tEnter your age : '))
+hobby=input('\n\tEnter your favourite hobby : ')
+print("\n\tHello, <",name,'> At <',age,'> enjoying <',hobby,'> sounds fun!')
+'''
+Output:
+
+	Enter your name : Neev
+
+	Enter your age : 18
+
+	Enter your favourite hobby : Cricket
+
+	Hello, < Neev > At < 18 > enjoying < Cricket > sounds fun!
+'''
