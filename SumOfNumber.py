b309bde3798b640e88a498fbb5592ffff85a92b4
Neev Shankar <nivshankar9@gmail.com>
2026-05-18 18:37:12 +0530

    Sum from 1 to n

---
+++ SumOfNumber.py
@@ -0,0 +1,13 @@
+n = int ( input ("Enter a number: "))
+Sum=0
+a=1
+while a<=n:
+    Sum=Sum+a
+    a+=1
+print("The sum of all numbers from 1 to n is: ",Sum)
+
+'''
+Output:
+Enter a number: 10
+The sum of all numbers from 1 to n is:  55
+'''
