d587919cf2d2d6b8026193122565314567bbcb4b
Neev Shankar <nivshankar9@gmail.com>
2026-05-19 18:46:39 +0530

    Pattern with space

--- Pattern7.py
+++ Pattern7.py
@@ -2,14 +2,14 @@ for i in range(1,6):
     for _ in range(5,i,-1):
         print(" ",end =" ")
     for j in range(1,i+1):
-        print(i,end=" ")
+        print(j,end=" ")
     print()
-    
+
 '''
 Output:
         1 
-      2 2 
-    3 3 3 
-  4 4 4 4 
-5 5 5 5 5
+      1 2 
+    1 2 3 
+  1 2 3 4 
+1 2 3 4 5 
 '''
