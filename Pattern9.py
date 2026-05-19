16a89c055dfd39d09c89b0d688dfbae9c2a8a17e
Neev Shankar <nivshankar9@gmail.com>
2026-05-19 18:53:41 +0530

    Pattern with space

---
+++ Pattern9.py
@@ -0,0 +1,14 @@
+for i in range(1,6):
+    for _ in range(5,i,-1):
+        print(end=" ")
+    for _ in range(1,i+1):
+        print("*",end=" ")
+    print()
+'''
+Output:
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+'''
