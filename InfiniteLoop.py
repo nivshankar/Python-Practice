c8438159708265febb5eaf82f504bd2714775f68
Neev Shankar <nivshankar9@gmail.com>
2026-05-20 20:02:59 +0530

    Loop continues until enter 0

---
+++ InfiniteLoop.py
@@ -0,0 +1,18 @@
+while True:
+    num=int(input('Enter number: '))
+    if num == 0:
+        break
+    else:
+        continue
+'''
+Output:
+Enter number: 4
+Enter number: 5
+Enter number: 7
+Enter number: 8
+Enter number: 9
+Enter number: 6
+Enter number: 4
+Enter number: 65
+Enter number: 0
+'''
