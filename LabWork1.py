8dc3428b4af125172f739ee175969b59496deb5e
Neev Shankar <nivshankar9@gmail.com>
2026-05-22 16:54:08 +0530

    Display last , first name

---
+++ LabWork1.py
@@ -0,0 +1,13 @@
+first_name=input('\n\tEnter your first name : ')
+last_name=input('\n\tEnter your last name : ')
+print("\n\tHello , {} , {}".format(last_name,first_name))
+
+'''
+Output:
+
+	Enter your first name : Neev
+
+	Enter your last name : shankar
+
+	Hello , shankar , Neev
+'''
