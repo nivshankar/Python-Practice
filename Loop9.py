e4382877ee79db89e2d0dc5514b4649c785f38a4
Neev Shankar <nivshankar9@gmail.com>
2026-05-22 16:39:21 +0530

    Divisible  by 2,3 or both

---
+++ Loop9.py
@@ -0,0 +1,44 @@
+for i in range(1,51):
+    if i%6==0:
+        print(f"{i} is divisible by both 2 and 3.")
+    elif i%2==0:
+        print(f"{i} is divisible by 2.")
+    elif i%3==0:
+        print(f"{i} is divisible by 3.")
+
+'''
+Output:
+2 is divisible by 2.
+3 is divisible by 3.
+4 is divisible by 2.
+6 is divisible by both 2 and 3.
+8 is divisible by 2.
+9 is divisible by 3.
+10 is divisible by 2.
+12 is divisible by both 2 and 3.
+14 is divisible by 2.
+15 is divisible by 3.
+16 is divisible by 2.
+18 is divisible by both 2 and 3.
+20 is divisible by 2.
+21 is divisible by 3.
+22 is divisible by 2.
+24 is divisible by both 2 and 3.
+26 is divisible by 2.
+27 is divisible by 3.
+28 is divisible by 2.
+30 is divisible by both 2 and 3.
+32 is divisible by 2.
+33 is divisible by 3.
+34 is divisible by 2.
+36 is divisible by both 2 and 3.
+38 is divisible by 2.
+39 is divisible by 3.
+40 is divisible by 2.
+42 is divisible by both 2 and 3.
+44 is divisible by 2.
+45 is divisible by 3.
+46 is divisible by 2.
+48 is divisible by both 2 and 3.
+50 is divisible by 2.
+'''
