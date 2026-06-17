da61341ff9b7ee1d51da286f9ee4b020927fb6de
Neev Shankar <nivshankar9@gmail.com>
2026-06-17 21:35:51 +0530

    The module used for 2.py

---
+++ math_utils.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    print(f"\nAdddition of {a} and {b} is {a+b}")
+def subtract(a,b):
+    print(f"\nSubtracttion of {a} and {b} is {a-b}")
+def multiply(a,b):
+    print(f"\nSubtracttion of {a} and {b} is {a*b}")
+def divide(a,b):
+    print(f"\nDivision of {a} and {b} is {a/b}")
