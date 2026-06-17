3d741b43f6d30452f083180581c344c925129e80
Neev Shankar <nivshankar9@gmail.com>
2026-06-17 21:46:11 +0530

    Use of module calculate between 2 numbers

--- 2.py
+++ 2.py
@@ -3,3 +3,14 @@ m.add(10,20)
 m.subtract(34,5)
 m.multiply(6,17)
 m.divide(45,7)
+'''
+Output:
+
+Adddition of 10 and 20 is 30
+
+Subtracttion of 34 and 5 is 29
+
+Subtracttion of 6 and 17 is 102
+
+Division of 45 and 7 is 6.428571428571429
+'''
