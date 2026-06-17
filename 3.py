577676dadee63acbb63b0169bfb856ec67d30da2
Neev Shankar <nivshankar9@gmail.com>
2026-06-17 22:04:59 +0530

    Two methods of math modules used

---
+++ 3.py
@@ -0,0 +1,10 @@
+import math as m
+print(f"\nSquare root of 49 is {m.sqrt(49)}")
+print(f"\nRadian equivalent to 90 degree is {m.radians(90)}")
+'''
+Output:
+
+Square root of 49 is 7.0
+
+Radian equivalent to 90 degree is 1.5707963267948966
+'''
